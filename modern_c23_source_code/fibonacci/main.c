19182e6fce6f3fa753abd0e6c525482f62781288
Dan Jakob Ofer <dan@ofer.to>
2025-01-12 16:06:09 -0500

    fibonacci: use a buffer

--- modern_c23_source_code/fibonacci/main.c
+++ modern_c23_source_code/fibonacci/main.c
@@ -5,23 +5,67 @@
 # define nullptr NULL
 #endif
 
-size_t fib(size_t n) {
-  if (n < 3)
+size_t fib(size_t n, int* buffer) {
+  if (buffer[n] != 0) {
+    printf("using buffer %zu\n", n);
+
+    return buffer[n];
+  }
+
+  printf("not using buffer %zu\n", n);
+
+  if (n < 3) {
+    buffer[n] = 1;
+
     return 1;
-  else
-    return fib(n-1) + fib(n-2);
+  } else {
+    size_t result = fib(n-1, buffer) + fib(n-2, buffer);
+
+    buffer[n] = result;
+
+    return result;
+  }
 }
 
+size_t max(size_t* numbers, size_t length);
 
 int main(int argc, char *argv[]) {
   div_t result = div(5, 2);
 
   printf("Result is %d / %d\n", result.quot, result.rem);
 
-  for (int i = 1; i < argc; ++i) {             // Processes args
+  size_t fibonacci_sequences_length = argc - 1;
+  size_t* fibonacci_sequences = malloc(argc * sizeof(size_t));
+
+  for (int i = 1; i < argc; ++i) {
     size_t const n = strtoull(argv[i], nullptr, 0);  // arg -> size_t
-    printf("fib(%zu) is %zu\n",
-           n, fib(n));
+    fibonacci_sequences[i - 1] = n;
   }
+
+  printf("There are %d argc\n", argc);
+  size_t maximum_fibonacci_number = max(fibonacci_sequences,
+fibonacci_sequences_length);
+  int* buffer = malloc(sizeof(size_t) * maximum_fibonacci_number);
+
+  for (size_t i = 0; i < fibonacci_sequences_length; i += 1) {
+    size_t length = fibonacci_sequences[i];
+    size_t result = fib(length, buffer);
+
+    printf("fib(%zu) is %zu\n", length, result);
+  }
+
+  free(fibonacci_sequences);
   return EXIT_SUCCESS;
+}
+
+size_t max(size_t* numbers, size_t length) {
+  size_t max_number = 0;
+
+  for (size_t i = 0; i < length; i += 1) {
+    if (numbers[i] > max_number) {
+      max_number = numbers[i];
+    }
+  }
+
+  return max_number;
 }
