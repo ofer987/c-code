a0f2824bdde9e33f62730a98d4bf755dc892e7d8
Dan Jakob Ofer <dan@ofer.to>
2025-01-25 12:11:11 -0500

    wip

--- snake/main.c
+++ snake/main.c
@@ -2,10 +2,49 @@
 #include <curses.h>
 #include <signal.h>
 
+const size_t SCREEN_SIZE = 256;
+
+enum screen_state {
+  AVAILABLE = 0,
+  USED_BY_SNAKE,
+  USED_BY_SNAKE_HEAD,
+  USED_BY_FOOD
+};
+enum snake_movement { LEFT = 0, UP, RIGHT, DOWN };
+
+struct snake_struct {
+  size_t head;
+  size_t size;
+  enum snake_movement current_movement;
+  /* size_t *locations; */
+};
+
+struct screen_struct {
+  size_t size;
+  size_t screen_state[SCREEN_SIZE];
+};
+
+struct screen_struct screen_struct;
+struct snake_struct snake_struct;
+
 static void finish(int sig);
+
+void update_screen_state(
+    enum screen_state *screen_states,
+    struct snake_struct *snake);
+
+void render_screen(enum screen_state *screen_states);
 
 int main(int argc, char *argv[]) {
   int num = 0;
+
+  enum screen_state screen_states[SCREEN_SIZE];
+
+  struct snake_struct snake = {
+    .head = SCREEN_SIZE / 2,
+    .size = 1
+      /* .locations = screen[SCREEN_SIZE / 2] */
+  };
 
   /* initialize your non-curses data structures here */
   (void) signal(SIGINT, finish);
@@ -36,19 +75,44 @@ int main(int argc, char *argv[]) {
      * necessary:
      */
     use_default_colors();
-    init_pair(1, COLOR_RED,     COLOR_WHITE);
-    init_pair(2, COLOR_GREEN,   COLOR_RED);
-    init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
-    init_pair(4, COLOR_BLUE,    COLOR_BLACK);
-    init_pair(5, COLOR_CYAN,    COLOR_BLACK);
-    init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(7, COLOR_WHITE,   COLOR_BLACK);
+    // Use default color for available space (i.e., `0`)
+    init_pair(1, COLOR_WHITE, COLOR_WHITE);
+    init_pair(2, COLOR_RED,   COLOR_RED);
+    init_pair(3, COLOR_BLACK, COLOR_BLACK);
   }
 
+  /* size_t screen_size[SCREEN_SIZE]; */
+
   for (;;) {
-    char ch = getch();     /* refresh, accept single keystroke of input */
+    size_t ch = getch();     /* refresh, accept single keystroke of input */
+    switch (ch) {
+      case KEY_LEFT:
+      case 'h':
+        snake.current_movement = LEFT;
+
+        break;
+      case KEY_UP:
+      case 'k':
+        snake.current_movement = UP;
+
+        break;
+      case KEY_RIGHT:
+      case 'l':
+        snake.current_movement = RIGHT;
+
+        break;
+      case KEY_DOWN:
+      case 'j':
+        snake.current_movement = DOWN;
+
+        break;
+    }
+
     attrset(COLOR_PAIR(num % 8));
     num += 1;
+
+    update_screen_state(screen_states, &snake);
+    render_screen(screen_states);
 
     if (ch == 'q') {
       finish(0);
@@ -67,3 +131,36 @@ static void finish(int sig) {
 
   exit(EXIT_SUCCESS);
 }
+
+void update_screen_state(
+    enum screen_state *screen_states,
+    struct snake_struct *snake) {
+  // TODO(ofer987): fix later
+  // Should be based on the **relative location** and
+  // the snake's **current_movement**
+  if (screen_states[snake->head] != USED_BY_FOOD) {
+    return;
+  }
+
+  snake->size += 1;
+  screen_states[snake->head] = USED_BY_SNAKE;
+
+  return;
+}
+
+// TODO(ofer987): should I use an array instead?
+void render_screen(enum screen_state *screen_states) {
+  for (size_t i = 0; i < SCREEN_SIZE; i += 1) {
+    attrset(COLOR_PAIR(screen_states[i]));
+    /* attr_set(COLOR_PAIR((int) (screen_states[i]))); */
+
+    /* attr_g, cp, o); */
+    /* switch (screen_states[i]) { */
+    /*   case AVAILABLE: */
+    /*     attrset(COLOR_PAIR(0)); */
+    /*  */
+    /*     break; */
+    /*   case USED_BY_SNAKE: */
+    /*     attrset(COLOR_PAIR(USED)); */
+  }
+  }
